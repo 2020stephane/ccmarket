5d33a2c0aac693520d1aabdb3fcaad69819a976b
2020stephane <stephanebrisse@gmail.com>
2026-03-25 16:33:11 +0100

    delete and modifie files

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 const annoncesRouter = require("./routes/annonces");
 
 const app = express();
@@ -8,24 +9,26 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 app.use("/api/annonces", annoncesRouter);
-app.use(express.static(path.join(__dirname, 'html')));
-app.use('/css', express.static(path.join(__dirname, 'css')));
-app.use('/js', express.static(path.join(__dirname, 'js')));
-app.use('/img', express.static(path.join(__dirname, 'img')));
-app.use('/fonts', express.static(path.join(__dirname, 'fonts')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
- 
+
+// __dirname = ccmarket/server, donc on remonte d'un niveau avec '..'
+app.use(express.static(path.join(__dirname, '..', 'html')));
+app.use('/css', express.static(path.join(__dirname, '..', 'css')));
+app.use('/js', express.static(path.join(__dirname, '..', 'js')));
+app.use('/img', express.static(path.join(__dirname, '..', 'img')));
+app.use('/fonts', express.static(path.join(__dirname, '..', 'fonts')));
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
 // Route principale → index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'html', 'index.html'));
 });
- 
+
 // Gérer les autres routes HTML (si tu as plusieurs pages)
 app.get('*', (req, res) => {
-  const filePath = path.join(__dirname, 'html', req.path);
+  const filePath = path.join(__dirname, '..', 'html', req.path);
   res.sendFile(filePath, (err) => {
     if (err) {
-      res.status(404).sendFile(path.join(__dirname, 'html', 'index.html'));
+      res.status(404).sendFile(path.join(__dirname, '..', 'html', 'index.html'));
     }
   });
 });
